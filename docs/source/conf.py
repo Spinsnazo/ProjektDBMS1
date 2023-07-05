5b2f389f38bc7cd615250cd26acf026ab644d55d
ErykMika <264451@student.pwr.edu.pl>
2023-07-05 11:15:00 +0200

    Improve docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ language = 'pl'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_title = 'Sprawozdanie'
 html_use_index = False
